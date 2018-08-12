8ed8c08cc928e305a3055df37715170d003e5ddb
Aleksey <33554901+AlexeyYan@users.noreply.github.com>
2018-08-13 00:36:33 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+import asyncio
+client = discord.Client()
+@client.event
+async def on_ready():
+ print('Logged on')
+ print(client.user.name)
+ print(client.user.id)
+ print("-------------")
+ @client.event
+async def on_message(message):  
+  if message.content.startswith('!isalexcool'):
+         await client.send_message(message.channel,'Of course!')
+  client.run('NDI2MDE1MzAxOTUyMzM5OTY4.DZQBlg.bCwyfXTtRmoHlvw6w8W2xo9OooY')
