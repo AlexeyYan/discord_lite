6791f7804e5d07e002917316b75c1d95d35ee504
Aleksey <33554901+AlexeyYan@users.noreply.github.com>
2018-08-13 00:21:52 +0300

    Update discordbot_lite.py

--- discordbot_lite.py
+++ discordbot_lite.py
@@ -1,16 +1,1 @@
-import discord
-import asyncio
-client = discord.Client()
-@client.event
-async def on_ready():
- print('Logged on')
- print(client.user.name)
- print(client.user.id)
- print("-------------")
-
-@client.event
-async def on_message(message):  
-  if message.content.startswith('!isalexcool'):
-         await client.send_message(message.channel,'Of course!')
-  client.run('NDI1Njk5NDM5NTk3MDYwMDk2.DZVeoA.ErWcwG-mu7w6a9IfaJCPUOSyOwo')
 
