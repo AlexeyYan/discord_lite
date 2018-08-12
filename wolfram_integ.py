713d57425969b4a7f0a95e220fa44dbb71b1620f
Aleksey <33554901+AlexeyYan@users.noreply.github.com>
2018-08-13 01:46:55 +0300

    Create wolfram_integ.py

---
+++ wolfram_integ.py
@@ -0,0 +1,31 @@
+import wolframalpha                                
+import readline  
+import os
+
+app_id = os.environ['WOLF_ID']
+                        
+def Question(question):
+ client = wolframalpha.Client(app_id)               
+ res=client.query(question)
+ if res['@success']=='true':
+     answer = res['pod'][0]['subpod']['img']['@src']
+ else: answer = 'Упс, что-то пошло не так. Проверьте вопрос'
+ return answer                                     
+
+def Plot(question):
+    client=wolframalpha.Client(app_id)
+    res=client.query(question)                         
+    if res['@success']=='true':                            
+      answer = res['pod'][1]['subpod']                   
+      if len(answer)>=4:
+        answer=answer[1]['img']['@src']
+      else: answer=answer[1]['img']['@src']
+    else: answer = 'Упс, что-то пошло не так. Проверьте вопрос'
+    return answer
+                                                                                     
+def Solve (question):
+ client=wolframalpha.Client(app_id)
+ res=client.query(question)
+ if res['@success']=='true': answer = next(res.results).text
+ else: answer = 'Упс, что-то пошло не так. Проверьте вопрос'
+ return answer
