0c045a0c60c873ad41c956a9c2d30eddf40c95c1
Aleksey <33554901+AlexeyYan@users.noreply.github.com>
2018-08-13 00:45:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ async def on_ready():
  print(client.user.name)
  print(client.user.id)
  print("-------------")
- @client.event
+ 
+@client.event
 async def on_message(message):  
   if message.content.startswith('!isalexcool'):
          await client.send_message(message.channel,'Of course!')
-  client.run('NDI2MDE1MzAxOTUyMzM5OTY4.DZQBlg.bCwyfXTtRmoHlvw6w8W2xo9OooY')
+client.run('NDI2MDE1MzAxOTUyMzM5OTY4.DZQBlg.bCwyfXTtRmoHlvw6w8W2xo9OooY')
