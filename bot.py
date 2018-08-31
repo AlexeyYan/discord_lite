76f3f1f53dd80086f511a2ae9bff3dfae95f41df
AlexeyYan <33554901+AlexeyYan@users.noreply.github.com>
2018-08-31 20:33:55 +0300

    Mmmm, love ,

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ async def on_message(message):
  elif message.content.startswith('!curs'):
         if message.content[6:]=='all':
             curs=Curs_All()
-            await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.green(), title='***Основные курсы валют на сегодня:***' description=curs)
+            await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.green(), title='***Основные курсы валют на сегодня:***', description=curs)
         else:
             curs=Curs()
             await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.green(), title='***Курсы валют на сегодня:***' description=curs)
