e6544198ff0ec0c032f73ce7c4800c6c3563328b
AlexeyYan <33554901+AlexeyYan@users.noreply.github.com>
2018-09-05 19:09:35 +0300

    fix bugs with PE lesson

--- schedules.py
+++ schedules.py
@@ -10,7 +10,10 @@ def getTodaySchedules():
              if week in lesson['weekNumber'] or r.json()['todaySchedules']!=None:
                  times=('Время: ' + lesson['lessonTime'])
                  sub=('Предмет: ' + lesson['subject']+' ('+lesson['lessonType']+')')
-                 employeer=('Препод: ' + lesson['employee'][0]['fio'])
+                 if lesson['employee'][0]['fio']:
+                     employeer=('Препод: ' + lesson['employee'][0]['fio'])
+                 else:
+                     employeer=''
                  aud=('Аудитория: ' + lesson['auditory'][0])
                  answer+=times+'\n'+sub+'\n'+aud+'\n'+employeer+'\n-----------------\n'
           return answer
@@ -26,7 +29,10 @@ def getTomorrowSchedules():
                if week in lesson['weekNumber'] or r.json()['tomorrowSchedules']!=None:
                    times=('Время: ' + lesson['lessonTime'])
                    sub=('Предмет: ' + lesson['subject']+' ('+lesson['lessonType']+')')
-                   employeer=('Препод: ' + lesson['employee'][0]['fio'])
+                   if lesson['employee'][0]['fio']:
+                     employeer=('Препод: ' + lesson['employee'][0]['fio'])
+                   else:
+                     employeer=''
                    aud=('Аудитория: ' + lesson['auditory'][0])
                    answer+=times+'\n'+sub+'\n'+aud+'\n'+employeer+'\n-----------------\n'
          return answer
