5d41349309e36b023009a94cb16cc831da5f22bb
AlexeyYan <33554901+AlexeyYan@users.noreply.github.com>
2019-02-18 23:03:44 +0300

    v 1.0 Release:   - Add some OOP   - refactoring

--- bot.py
+++ bot.py
@@ -6,136 +6,139 @@ import os
 import requests
 import json
 import time
+from datetime import datetime
 import helps
-from vk_integ import *
+from vk_integ import Vk_Integration
 from imgur_integ import *
 from wolfram_integ import *
 from funs import *
 from accuw_integ import *
 from games import *
 from schedules import *
-from datetime import datetime
 
-discord_token=os.environ['DISCORD_TOKEN']
-            
+discord_token = os.environ['DISCORD_TOKEN']
 client = discord.Client()
-GAMES=['Skynet', 'программирование', 'кубики', '*не играет*', 'CS:GO', 'рок группе', 'песочнице', 'пьесе']
-i=0
-async def Kostil():
-        await client.wait_until_ready()
-        await client.send_message(458335056532996097, i)
-        i+=1
-        await asyncio.sleep(10)
+vk = Vk_Integration()
+GAMES = ['Skynet', 'программирование', 'кубики',
+         '*не играет*', 'CS:GO', 'рок группе', 'песочнице', 'пьесе']
+
 
 @client.event
 async def on_ready():
- await client.change_presence(game=discord.Game(name=random.choice(GAMES), status = discord.Status.idle))
- print('Logged on')
- print(client.user.name)
- print(client.user.id)
- print("-------------")
-                  
+    await client.change_presence(game=discord.Game(name=random.choice(GAMES), status=discord.Status.idle))
+    print('Logged on')
+    print(client.user.name)
+    print(client.user.id)
+    print("-------------")
+
+
 @client.event
 async def on_message(message):
- if message.content.startswith('!ping'):
-    await client.send_message(message.channel, 'pong')
- 
- elif message.content.startswith('!rasp'):
-      if message.content.startswith('!rasp next'):
-         answer=getTomorrowSchedules()
-      else:
-         answer=getTodaySchedules()
-      await client.send_message(message.channel, answer)
+    if message.content.startswith('!ping'):
+        await client.send_message(message.channel, 'pong')
 
+    elif message.content.startswith('!rasp'):
+        if message.content.startswith('!rasp next'):
+            answer = getTomorrowSchedules()
+        else:
+            answer = getTodaySchedules()
+        await client.send_message(message.channel, answer)
 
- elif message.content.startswith('!randvk'):
+    elif message.content.startswith('!randvk'):
         if message.content.startswith('!randvk bntu'):
-         mem=Random_BNTU_Mem()
+            mem = vk.random_bntu_mem()
         else:
-          if message.content.startswith('!randvk it'):
-           mem=Random_IT_Mem()
-          else:
-           mem=Random_VkMem()
-        await client.send_message(message.channel,mem)
+            if message.content.startswith('!randvk it'):
+                mem = vk.random_it_mem()
+            else:
+                mem = vk.random_mem()
+        await client.send_message(message.channel, mem)
 
- elif message.content.startswith('!randpic'):
-      pic = Random_Pic()
-      await client.send_message(message.channel,pic)
+    elif message.content.startswith('!randpic'):
+        pic = Random_Pic()
+        await client.send_message(message.channel, pic)
 
- elif message.content.startswith('!joke'):
-         joke = Random_Joke()
-         await client.send_message(message.channel,joke)
+    elif message.content.startswith('!joke'):
+        joke = vk.random_joke()
+        await client.send_message(message.channel, joke)
 
- elif message.content.startswith('!wolf'):
-         question=str(message.content[6:])
-         answer = Question(question)
-         await client.send_message(message.channel,answer)
+    elif message.content.startswith('!wolf'):
+        question = str(message.content[6:])
+        answer = Question(question)
+        await client.send_message(message.channel, answer)
 
- elif message.content.startswith('!dice'):
-         cube1, cube2 = Dice()
-         name=message.author.name
-         await client.send_message(message.channel, name+': выпало '+cube1+' и '+cube2)
+    elif message.content.startswith('!dice'):
+        cube1, cube2 = Dice()
+        name = message.author.name
+        await client.send_message(message.channel, name+': выпало '+cube1+' и '+cube2)
 
- elif message.content.startswith('!flip'):
-         if random.randint(0,1000)<=453 : ans = 'Орёл'
-         else: ans = 'Решка'
-         await client.send_message(message.channel,'Выпало: '+ans)
-
- elif message.content.startswith('!weather'):
-         result=Daily_Forecast()
-         await client.send_message(message.channel,result)
-
- elif message.content.startswith('!curs'):
-        if message.content[6:]=='all':
-            curs=Curs_All()
-            await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.green(), title='***Основные курсы валют на сегодня:***', description=curs))
+    elif message.content.startswith('!flip'):
+        if random.randint(0, 1000) <= 453:
+            ans = 'Орёл'
         else:
-            curs=Curs()
-            await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.green(), title='***Курсы валют на сегодня:***', description=curs))
- 
- elif message.content.startswith('!inst'):
-         url = message.content[6:]
-         answer = getInstagramContent(url)
-         await client.send_message(message.channel, answer)
-         await client.delete_message(message)
- 
- elif message.content.startswith('!qr'):
-         value=message.content[4:]
-         createQRCode(value)
-         qr=open('qr.jpg', 'rb')
-         await client.send_file(message.author,qr)
-         await client.delete_message(message)
+            ans = 'Решка'
+        await client.send_message(message.channel, 'Выпало: '+ans)
 
- elif message.content.startswith('!help'):
-        if message.content[6:] !='':
-                if message.content[6:] in helps.Commands_richList.keys():
-                        answer=discord.Embed(color=discord.Color.blue(), title='***'+message.content[6:]+' - help***', description=helps.Commands_richList[message.content[6:]])
-                else:
-                        answer = discord.Embed(color=discord.Color.red(), title='__***Error!***__', description='Команда не найдена')
+    elif message.content.startswith('!weather'):
+        result = Daily_Forecast()
+        await client.send_message(message.channel, result)
+
+    elif message.content.startswith('!curs'):
+        if message.content[6:] == 'all':
+            curs = Curs_All()
+            await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.green(), title='***Основные курсы валют на сегодня:***', description=curs))
         else:
-                answer = discord.Embed(color = discord.Color.blue(), title='***Commands List***', description = helps.Commands_List)
+            curs = Curs()
+            await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.green(), title='***Курсы валют на сегодня:***', description=curs))
+
+    elif message.content.startswith('!inst'):
+        url = message.content[6:]
+        answer = getInstagramContent(url)
+        await client.send_message(message.channel, answer)
+        await client.delete_message(message)
+
+    elif message.content.startswith('!qr'):
+        value = message.content[4:]
+        createQRCode(value)
+        qr = open('qr.jpg', 'rb')
+        await client.send_file(message.author, qr)
+        await client.delete_message(message)
+
+    elif message.content.startswith('!help'):
+        if message.content[6:] != '':
+            if message.content[6:] in helps.Commands_richList.keys():
+                answer = discord.Embed(color=discord.Color.blue(
+                ), title='__***'+message.content[6:]+' - help***__', description=helps.Commands_richList[message.content[6:]])
+            else:
+                answer = discord.Embed(color=discord.Color.red(
+                ), title='__***Error!***__', description='Команда не найдена')
+        else:
+            answer = discord.Embed(color=discord.Color.blue(
+            ), title='__***Commands List***__', description=helps.Commands_List)
         await client. send_message(message.author, embed=answer)
         await client.delete_message(message)
 
- elif message.content.startswith('!pz'):
-         story=Pozor_Story()
-         await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.gray(), title='***Позор***', description=story))
+    elif message.content.startswith('!pz'):
+        story = vk.pozor_story()
+        await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.gray(), title='___***Позор***___', description=story))
 
+    elif message.content.startswith('!test'):
+        await client.send_message(message.channel, embed=discord.Embed(color=discord.Color.blue(), description='Test color'))
 
- elif message.content.startswith('!test'):
-        await client.send_message(message.channel,embed=discord.Embed(color=discord.Color.blue(), description='Test color'))
- 
- elif message.content.startswith('!stat'):
+    elif message.content.startswith('!stat'):
         await client.change_presence(game=discord.Game(name=random.choice(GAMES), type=0))
+
 
 @client.event
 async def on_message_delete(message):
-        print('User{} delete message: {}'.format(message.author.name, message.content))
+    print('User{} delete message: {}'.format(
+        message.author.name, message.content))
+
 
 @client.event
 async def on_member_join(member):
-        await client.send_message(member, 'Приветсвую {} на нашем сервере {}!'.format(member.name, server.name))
-        await client.send_message(447158757588205568, '{} вступил в нашу команду, поделитесь печеньками)'.fomat(member.name))
+    await client.send_message(member, 'Приветсвую {} на нашем сервере {}!'.format(member.name, server.name))
+    await client.send_message(447158757588205568, '{} вступил в нашу команду, поделитесь печеньками)'.fomat(member.name))
 
 client.loop.create_task(Kostil())
 client.run(discord_token)
--- vk_integ.py
+++ vk_integ.py
@@ -2,49 +2,44 @@ import vk
 import random
 import os
 
-vk_token = os.environ['VK_TOKEN']
 
-def Random_VkMem():
- session=vk.Session(vk_token)
- api = vk.API(session)
- offset=random.randint(0, 1000)
- r=api.wall.get(owner_id='-65596623', count=100, offset=offset, v='5.7')
- r=random.choice(r['items'])
- url=r['attachments'][0]['photo']['photo_807']
- return url
+class Vk_Integration(object):
+   def __init__(self):
+       self.vk_token = os.environ['VK_TOKEN']
+       self.session = vk.Session(vk_token)
+       self.api = vk.API(session)
+       self.v='5.7'
 
-def Random_Joke():
- session=vk.Session(vk_token)
- api = vk.API(session)
- offset=random.randint(0, 1000)
- r=api.wall.get(owner_id=-22222333, count=100, offset=offset, v='5.7')
- r=random.choice(r['items'])
- url=r['attachments'][0]['photo']['photo_807']
- return url
+   def random_mem(self):
+     offset = random.randint(0, 1000)
+     r = self.api.wall.get(owner_id='-65596623', count=100, offset=offset, v=self.v)
+     r = random.choice(r['items'])
+     url = r['attachments'][0]['photo']['photo_807']
+     return url
 
-def Random_BNTU_Mem():
- session=vk.Session(vk_token)
- api = vk.API(session)
- r=api.photos.get(owner_id=-59496516, album_id= 247176525, v='5.7')
- r=random.choice(r['items'])
- url = r['photo_604']
- return url
+   def random_joke():
+      offset = random.randint(0, 1000)
+      r = self.api.wall.get(owner_id=-22222333, count=100, offset=offset, v=self.v)
+      r = random.choice(r['items'])
+      url = r['attachments'][0]['photo']['photo_807']
+      return url
 
-def Random_IT_Mem():
- session=vk.Session(vk_token)
- api = vk.API(session)
- offset=random.randint(0, 100)
- r=api.wall.get(owner_id=-80799846, count=100, offset=offset, v='5.7')
- r=random.choice(r['items'])
- url=r['attachments'][0]['photo']['photo_604']
- return url
+   def random_bntu_mem():
+      r = self.api.photos.get(owner_id=-59496516, album_id=247176525, v=self.v)
+      r = random.choice(r['items'])
+      url = r['photo_604']
+      return url
 
- def Pozor_Story():
-    session=vk.Session(vk_token)
-    api = vk.API(session)
-    offset=random.randint(0, 1000)
-    r=api.wall.get(owner_id=-71729358, count=100, offset=offset, v='5.7')
-    r=random.choice(r['items'])
-    story=r['text']
-    return story
+   def random_it_mem():
+     offset = random.randint(0, 100)
+     r = self.api.wall.get(owner_id=-80799846, count=100, offset=offset, v=self.v)
+     r = random.choice(r['items'])
+     url = r['attachments'][0]['photo']['photo_604']
+     return url
 
+   def pozor_story():
+       offset = random.randint(0, 1000)
+       r = self.api.wall.get(owner_id=-71729358, count=100, offset=offset, v=self.v)
+       r = random.choice(r['items'])
+       story = r['text']
+       return story
